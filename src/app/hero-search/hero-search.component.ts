558cdf7a3f7bd234b899c7fdd28913fa3fba81dd
Ivan Kuzmin <kuzmin6969@gmail.com>
2018-06-04 12:06:20 +0300

    Relation field skeleton

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { Observable, Subject } from 'rxjs';
 
@@ -9,14 +9,19 @@ import {
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
+type RelationSearchMode = 'autocomplete' | 'table';
 @Component({
-  selector: 'app-hero-search',
+  selector: 'relation-input',
   templateUrl: './hero-search.component.html',
   styleUrls: [ './hero-search.component.css' ]
 })
 export class HeroSearchComponent implements OnInit {
+
   heroes$: Observable<Hero[]>;
   private searchTerms = new Subject<string>();
+
+  @Input()
+  public mode: RelationSearchMode = 'autocomplete';
 
   constructor(private heroService: HeroService) {}
 
@@ -25,14 +30,16 @@ export class HeroSearchComponent implements OnInit {
     this.searchTerms.next(term);
   }
 
+  showSearchTable(): void {
+    alert(32);
+  }
+
   ngOnInit(): void {
     this.heroes$ = this.searchTerms.pipe(
       // wait 300ms after each keystroke before considering the term
       debounceTime(300),
-
       // ignore new term if same as previous term
       distinctUntilChanged(),
-
       // switch to new search observable each time the term changes
       switchMap((term: string) => this.heroService.searchHeroes(term)),
     );
